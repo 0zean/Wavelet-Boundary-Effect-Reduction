5be44761f81d79be3c0e9a5553ba1a84971a865b
Nicholas <42080997+Nicholas-Picini@users.noreply.github.com>
2020-05-07 20:40:24 -0400

    Update EPFMODWT.R

--- EPFMODWT.R
+++ EPFMODWT.R
@@ -6,13 +6,6 @@ library(earth)
 setwd("C:/Users/Nick/Desktop/Market")
 set.seed(123)
 
-# get stock data from yahoo-finance
-#symbol <- "SPY"  
-#series <- new.env()
-#getSymbols.yahoo(symbol, env = series, from="2009-01-01", to=Sys.Date() + 1, auto.assign=T) 
-#stock <- series[[symbol]]
-#stock <- stock[, "SPY.Close"]
-
 esdf <- read.csv("emini.csv")
 es <- esdf["Close"]
 
@@ -35,8 +28,8 @@ flat <- EPF(signal)
 ts.plot(as.ts(flat))
 
 
-# sliding window wavelet transform saving last 2 coeffs from each window
-# ep is last coeff epk is one before ep
+# sliding window wavelet denoise saving last 2 coeffs from each window
+# ep is last coeff epk is second to last
 slideFunct <- function(data, window, step){
   total <- length(data)
   spots <- seq(from=1, to=(total-window), by=step)
@@ -60,7 +53,7 @@ vals = slideFunct(signal, 800, 1)
 plot.ts(vals)
 
 
-# sequentially sum the differences of endpoints from each sliding window
+# sequentially sum the differences of endpoints from each sliding window in a recursive fashion
 pre <- vals$ep - vals$epk
 vect <- vector(length = 1)*0
 
@@ -69,54 +62,10 @@ for(i in 1:length(pre)){
 }
 
 
+# plot last 100 prices from the original signal and the filter
 test <- signal[(length(signal)-99):length(signal)]
 test2 <- vect[(length(vect)-99):length(vect)]
 
 par(mfrow=c(2,1)) 
 plot.ts(test)
 plot.ts(test2)
-
-
-src <- vect
-src <- embed(src, 5)
-src <- tail(src, 16000)
-src <- data.frame(src)
-
-ss <- tail(signal, 16000)
-
-lbls = data.frame(ss)
-lbls_dt = seq(nrow(lbls)*.7)
-trnLbls <- lbls[lbls_dt, ]
-chkLbls <- lbls[-lbls_dt, ]
-  
-op <- tail(log(esdf["Open"]), 16000)
-  
-data <- src[,-1]
-data <- data.frame(data)
-data$open <- op
-data <- as.matrix(data)
-  
-data_dt = seq(nrow(data)*.7)
-trnData <- data[data_dt, ]
-chkData <- data[-data_dt, ]
-
-train <- data.frame(trnLbls, trnData)
-trainRow <- sample(nrow(train), replace = FALSE)
-train <- train[trainRow, ]
-
-t.lbls <- train[,1,drop=FALSE]
-t.data <- train[,-1]
-
-filter <- earth(x=t.data, y=t.lbls, degree=7)
-
-pred <- predict(filter, trnData)
-
-
-data.train <- data.frame(pred)
-data.train$actual <- trnLbls
-
-plot_ly(data.train, y = ~trnLbls, name = 'Predicted', type = 'scatter', mode = 'lines+markers') %>%
-  add_trace(y = ~actual, name = 'Actual', mode = 'lines+markers') %>%
-  layout(title = "EPF-MARS (In Sample)",
-         xaxis = list(title = "Time-Step (5 mins)"),
-         yaxis = list (title = "Log Price"))
