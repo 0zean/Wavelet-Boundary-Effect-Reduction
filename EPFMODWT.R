75a0bc7a51ec1a8abd371d868e96979bc47c7382
Nicholas Picini <42080997+0zean@users.noreply.github.com>
2023-06-06 00:32:36 -0400

    Update EPFMODWT.R

--- EPFMODWT.R
+++ EPFMODWT.R
@@ -2,19 +2,25 @@ library(wmtsa)
 library(ggplot2)
 library(quantmod)
 library(plotly)
+library(ggplot2)
+library(alphavantager)
 
-setwd("Wavelet-Boundary-Effect-Reduction/")
+
+setwd("../Wavelet-Boundary-Effect-Reduction/")
 set.seed(42)
 
-# Read csv and keep only the close price
-esdf <- read.csv("emini.csv")
-es <- esdf["Close"]
+# get stock data from alphavantage API
+key <- readLines("key.txt") # read api key from text file
+av_api_key(key)
+data <- av_get("AAPL", av_fun = "TIME_SERIES_INTRADAY", interval = "5min", outputsize = 'full')
 
-# Log transform to reduce exponential trends
-signal = as.ts(log(es))
+
+#signal = as.ts(log(es))
+signal = rev(data$close)
 ts.plot(signal)
 
-# End-point flattening fuction
+
+# End-point flattening function
 EPF <- function(data) {
   a = data[1]
   b = (data[length(data)] - a) / (length(data)-1)
@@ -26,22 +32,35 @@ EPF <- function(data) {
 }
 
 
+signal <- as.ts(rev(data$close))
 flat <- EPF(signal)
 ts.plot(as.ts(flat))
 
 
-# sliding window wavelet denoise saving last 2 coeffs from each window
+data <- data.frame(
+  day = seq(length(flat)),
+  value = flat
+)
+
+p <- ggplot(data, aes(x=day, y=value)) +
+  geom_line() + 
+  xlab("")
+ggplotly(p)
+
+
+# sliding window wavelet transform saving last 2 coeffs from each window
+# ep is last coeff epk is one before ep
 slideFunct <- function(data, window, step){
   total <- length(data)
   spots <- seq(from=1, to=(total-window), by=step)
-  ep <- vector(length = total-window) # ep is last coeff
-  epk <- vector(length = total-window) # epk is second to last coeff
+  ep <- vector(length = total-window)
+  epk <- vector(length = total-window)
   
   for(i in 1:length(spots)){
     newdata <- data[spots[i]:(spots[i]+window)]
     ff <- EPF(newdata)
     wave <- wavShrink(ff, wavelet="s8", n.level=3, shrink.fun="soft", 
-                      thresh.fun="universal", xform="modwt", reflect=FALSE) # Wavelet shrinkage to obtain a single smoothed signal
+                      thresh.fun="universal", xform="modwt", reflect=FALSE)
     filt <- as.ts(wave)
     ep[i] <- filt[length(filt)]
     epk[i] <- filt[length(filt)-1]
@@ -50,11 +69,12 @@ slideFunct <- function(data, window, step){
   return(df)
 }
 
+
 vals = slideFunct(signal, 800, 1)
 plot.ts(vals)
 
 
-# sequentially sum the differences of endpoints from each sliding window in a recursive fashion
+# sequentially sum the differences of endpoints from each sliding window
 pre <- vals$ep - vals$epk
 vect <- vector(length = 1)*0
 
@@ -62,11 +82,45 @@ for(i in 1:length(pre)){
   vect[i] <- pre[i] + vect[length(vect)]
 }
 
+library(smooth)
+ma <-  SMA(vect, n=14)
 
-# plot last 100 prices from the original signal and the filter
-test <- signal[(length(signal)-99):length(signal)]
-test2 <- vect[(length(vect)-99):length(vect)]
+
+price <- signal[(length(signal)-200):length(signal)]
+epf_filter <- vect[(length(vect)-200):length(vect)]
 
 par(mfrow=c(2,1)) 
-plot.ts(test)
-plot.ts(test2)
+plot.ts(price)
+plot.ts(epf_filter)
+
+
+data2 <- data.frame(
+  day = seq(length(price)),
+  value1 = price,
+  value2 = epf_filter
+)
+
+
+range01 <- function(x){(x-min(x))/(max(x)-min(x))}
+
+
+ggplot(data2, aes(x=day)) +
+  geom_line( aes(y=range01(value1))) + 
+  geom_line( aes(y=range01(value2))) +
+  scale_y_continuous(
+    name = "Norm Values"
+  )
+
+
+data3 <- data.frame(
+  day = seq(201),
+  value1 = vect[(length(vect)-200):length(vect)],
+  value2 = ma[(length(ma)-200):length(ma)]
+)
+
+ggplot(data3, aes(x=day)) +
+  geom_line( aes(y=value1)) + 
+  geom_line( aes(y=value2)) +
+  scale_y_continuous(
+    name = "EPF"
+  )
